be161ddcce8459bac3e619d676f752893c998154
ozpv <39195175+ozpv@users.noreply.github.com>
2023-10-15 21:29:48 +0000

    add CMY and CMYK, and RGB to CMY/CMYK

--- color.cpp
+++ color.cpp
@@ -2,10 +2,14 @@
 #include <iostream>
 
 namespace Color {
+    class CMY;
+    class CMYK;
     class RGB;
     class HSL;
     class HSV;
     class HEX;
+    CMY RGB_To_CMY(RGB& rgb);
+    CMYK RGB_To_CMYK(RGB& rgb);
     RGB HSL_To_RGB(HSL& hsl);
     RGB HSV_To_RGB(HSV& hsv);
     HSL RGB_To_HSL(RGB& rgb);
@@ -14,6 +18,57 @@ namespace Color {
     HSV HSL_To_HSV(HSL& hsl);
     HEX RGB_To_HEX(RGB& rgb);
     HEX HSL_To_HEX(HSL& hsl);
+
+    class CMY {
+        public:
+            CMY() : Cyan(0.0), Magenta(0.0), Yellow(0.0) {}
+            CMY(double cyan, double magenta, double yellow) : Cyan(cyan > 1.0 ? 1.0 : cyan), Magenta(magenta > 1.0 ? 1.0 : magenta), Yellow(yellow > 1.0 ? 1.0 : yellow) {}
+            ~CMY() {}
+
+            void operator=(RGB& rgb) {
+                CMY cmy(RGB_To_CMY(rgb));
+                this->Cyan = cmy.Cyan;
+                this->Magenta = cmy.Magenta;
+                this->Yellow = cmy.Yellow;
+            }
+
+            friend std::ostream& operator<<(std::ostream& out, const CMY& cmy) {
+                out << cmy.Cyan << " " << cmy.Magenta << " " << cmy.Yellow;
+                return out;
+            }
+
+            friend CMY RGB_To_CMY(RGB& rgb);
+            friend CMYK RGB_To_CMYK(RGB& rgb);
+            friend CMYK;
+        private:
+            double Cyan;
+            double Magenta;
+            double Yellow;
+    };
+
+    class CMYK : private CMY {
+        public:
+            CMYK() : Key(0.0) { Cyan = 0.0; Magenta = 0.0; Yellow = 0.0; }
+            CMYK(double cyan, double magenta, double yellow, double key) : Key(key > 1.0 ? 1.0 : key) { Cyan = cyan > 1.0 ? 1.0 : cyan; Magenta = magenta > 1.0 ? 1.0 : magenta; Yellow = yellow > 1.0 ? 1.0 : yellow; }
+            ~CMYK() {}
+
+            void operator=(RGB& rgb) {
+                CMYK cmyk(RGB_To_CMYK(rgb));
+                this->Cyan = cmyk.Cyan;
+                this->Magenta = cmyk.Magenta;
+                this->Yellow = cmyk.Yellow;
+                this->Key = cmyk.Key;
+            }
+
+            friend std::ostream& operator<<(std::ostream& out, const CMYK& cmyk) {
+                out << cmyk.Cyan << " " << cmyk.Magenta << " " << cmyk.Yellow << " " << cmyk.Key;
+                return out;
+            }
+
+            friend CMYK RGB_To_CMYK(RGB& rgb);
+        private:
+            double Key;
+    };
 
     class RGB {
         public:
@@ -46,6 +101,8 @@ namespace Color {
                 return out;
             } 
 
+            friend CMY RGB_To_CMY(RGB& rgb);
+            friend CMYK RGB_To_CMYK(RGB& rgb);
             friend RGB HSL_To_RGB(HSL& hsl);
             friend RGB HSV_To_RGB(HSV& hsv);
             friend HSL RGB_To_HSL(RGB& rgb);
@@ -62,8 +119,8 @@ namespace Color {
 
     class HSL {
         public:
-            HSL() : Hue(0), Saturation(0), Lightness(0) {}
-            HSL(double hue, double saturation, double lightness) : Hue(hue > 360 ? 360 : hue), Saturation(saturation > 1 ? 1 : saturation), Lightness(lightness > 1 ? 1 : lightness) {}
+            HSL() : Hue(0.0), Saturation(0.0), Lightness(0.0) {}
+            HSL(double hue, double saturation, double lightness) : Hue(hue > 360.0 ? 360.0 : hue), Saturation(saturation > 1.0 ? 1.0 : saturation), Lightness(lightness > 1.0 ? 1.0 : lightness) {}
             ~HSL() {}
 
             void operator=(RGB& rgb) {
@@ -85,10 +142,10 @@ namespace Color {
                 return out;
             }
 
+            friend RGB HSL_To_RGB(HSL& hsl);
             friend HSL RGB_To_HSL(RGB& rgb);
             friend HSL HSV_To_HSL(HSV& hsv);
             friend HSV HSL_To_HSV(HSL& hsl);
-            friend RGB HSL_To_RGB(HSL& hsl);
         private:
             double Hue;
             double Saturation;
@@ -97,8 +154,8 @@ namespace Color {
 
     class HSV {
         public:
-            HSV() : Hue(0), Saturation(0), Value(0) {}
-            HSV(double hue, double saturation, double value) : Hue(hue > 360 ? 360 : hue), Saturation(saturation > 1 ? 1 : saturation), Value(value > 1 ? 1 : value) {}
+            HSV() : Hue(0.0), Saturation(0.0), Value(0.0) {}
+            HSV(double hue, double saturation, double value) : Hue(hue > 360.0 ? 360.0 : hue), Saturation(saturation > 1.0 ? 1.0 : saturation), Value(value > 1.0 ? 1.0 : value) {}
             ~HSV() {}
 
             void operator=(RGB& rgb) {
@@ -214,31 +271,69 @@ namespace Color {
         return hue;
     }
 
+    CMY RGB_To_CMY(RGB& rgb) {
+        CMY cmy;
+
+        double c = 1 - (static_cast<double>(rgb.Red) / 255);
+        double m = 1 - (static_cast<double>(rgb.Green) / 255);
+        double y = 1 - (static_cast<double>(rgb.Blue) / 255);
+
+        double min = tmin3<double>(m, y, c);
+
+        cmy.Cyan = (c - min) / (1 - min);
+        cmy.Magenta = (m - min) / (1 - min);
+        cmy.Yellow = (y - min) / (1 - min);
+
+        return cmy;
+    }
+
+    CMYK RGB_To_CMYK(RGB& rgb) {
+        CMYK cmyk;
+
+        double c = 1 - (static_cast<double>(rgb.Red) / 255);
+        double m = 1 - (static_cast<double>(rgb.Green) / 255);
+        double y = 1 - (static_cast<double>(rgb.Blue) / 255);
+
+        double min = tmin3<double>(m, y, c);
+
+        cmyk.Cyan = (c - min) / (1 - min);
+        cmyk.Magenta = (m - min) / (1 - min);
+        cmyk.Yellow = (y - min) / (1 - min);
+        
+        if (rgb.Red == 0 && rgb.Green == 0 && rgb.Blue == 0) {
+            cmyk.Key = 1.0;
+        } else {
+            cmyk.Key = min;
+        }
+
+        return cmyk;
+    }
+
     RGB HSL_To_RGB(HSL& hsl) {
         RGB rgb;
 
-		double k;
+	double k;
 
-		double a = hsl.Saturation * tmin<double>(hsl.Lightness, 1 - hsl.Lightness);
+	double a = hsl.Saturation * tmin<double>(hsl.Lightness, 1.0 - hsl.Lightness);
 
-		/* to catch our RGB values */
-		double rgbd[3];
+	/* to catch our RGB values */
+	double rgbd[3];
 
-		/* to calculate f(0) f(8) f(4) */
-		double n[3] = { 0.0, 8.0, 4.0 };
+	/* to calculate f(0) f(8) f(4) */
+	double n[3] = { 0.0, 8.0, 4.0 };
 
-		for (int i = 0; i < 3; i++) {
-			k = (n[i] + hsl.Hue / 30.0);
+	for (int i = 0; i < 3; i++) {
+		k = (n[i] + hsl.Hue / 30.0);
 
-			/* simple mod template */
-			k = tmod<double>(k, 12);
+		/* simple mod template */
+		k = tmod<double>(k, 12.0);
 
-			rgbd[i] = hsl.Lightness - a * tmax<double>(-1.0, tmin3<double>(k - 3.0, 9.0 - k, 1.0));
-		}
+		rgbd[i] = hsl.Lightness - a * tmax<double>(-1.0, tmin3<double>(k - 3.0, 9.0 - k, 1.0));
+	}
 
-		rgb.Red = static_cast<uint8_t>(rgbd[0] * 255.0);
-		rgb.Green = static_cast<uint8_t>(rgbd[1] * 255.0);
-		rgb.Blue = static_cast<uint8_t>(rgbd[2] * 255.0);
+	rgb.Red = static_cast<uint8_t>(rgbd[0] * 255.0);
+	rgb.Green = static_cast<uint8_t>(rgbd[1] * 255.0);
+	rgb.Blue = static_cast<uint8_t>(rgbd[2] * 255.0);
 
         return rgb;
     }
@@ -246,26 +341,26 @@ namespace Color {
     RGB HSV_To_RGB(HSV& hsv) {
         RGB rgb;
 
-		double k;
+	double k;
 
-		/* to catch our RGB values */
-		double rgbd[3];
+	/* to catch our RGB values */
+	double rgbd[3];
 
-		/* to calculate f(5) f(3) f(1) */
-		double n[3] = { 5.0, 3.0, 1.0 };
+	/* to calculate f(5) f(3) f(1) */
+	double n[3] = { 5.0, 3.0, 1.0 };
 
-		for (int i = 0; i < 3; i++) {
-			k = (n[i] + hsv.Hue / 60.0);
+	for (int i = 0; i < 3; i++) {
+		k = (n[i] + hsv.Hue / 60.0);
 
-			/* simple mod template */
-			k = tmod<double>(k, 6);
+		/* simple mod template */
+		k = tmod<double>(k, 6.0);
 
-			rgbd[i] = hsv.Value - (hsv.Value * hsv.Saturation * tmax<double>(0, tmin3<double>(k, 4 - k, 1)));
-		}
+		rgbd[i] = hsv.Value - (hsv.Value * hsv.Saturation * tmax<double>(0.0, tmin3<double>(k, 4.0 - k, 1.0)));
+	}
 
-		rgb.Red = static_cast<uint8_t>(rgbd[0] * 255.0);
-		rgb.Green = static_cast<uint8_t>(rgbd[1] * 255.0);
-		rgb.Blue = static_cast<uint8_t>(rgbd[2] * 255.0);
+	rgb.Red = static_cast<uint8_t>(rgbd[0] * 255.0);
+	rgb.Green = static_cast<uint8_t>(rgbd[1] * 255.0);
+	rgb.Blue = static_cast<uint8_t>(rgbd[2] * 255.0);
 
         return rgb;
     }
@@ -274,40 +369,40 @@ namespace Color {
         HSL hsl;
 
         /* scale our values to between [0, 1]
-		 * cast our vaules to T to ensure the compiler interprets it correctly
-		 */
-		double red = static_cast<double>(rgb.Red) / 255.0;
-		double green = static_cast<double>(rgb.Green) / 255.0;
-		double blue = static_cast<double>(rgb.Blue) / 255.0;
+	 * cast our vaules to T to ensure the compiler interprets it correctly
+	 */
+	double red = static_cast<double>(rgb.Red) / 255.0;
+	double green = static_cast<double>(rgb.Green) / 255.0;
+	double blue = static_cast<double>(rgb.Blue) / 255.0;
 
-		double max = tmax3<double>(red, green, blue);
-		double min = tmin3<double>(red, green, blue);
+	double max = tmax3<double>(red, green, blue);
+	double min = tmin3<double>(red, green, blue);
 
-		/* calculate chroma */
-		double chroma = max - min;
+	/* calculate chroma */
+	double chroma = max - min;
 
-		double hue;
-		double saturation;
+	double hue;
+	double saturation;
 
-		/* calculate the lightness value by averaging max and min */
-		double lightness = (max + min) / 2.0;
+	/* calculate the lightness value by averaging max and min */
+	double lightness = (max + min) / 2.0;
 
-		/* color is achromatic and we don't need to calculate the hue */
-		if (max == min) {
-			hue = 0.0;
-			saturation = 0.0;
-		} else {
-			/* calculate saturation */
-			saturation = (lightness > 0.5) ? chroma / (2.0 - max - min) : chroma / (max + min);
+	/* color is achromatic and we don't need to calculate the hue */
+	if (max == min) {
+		hue = 0.0;
+		saturation = 0.0;
+	} else {
+		/* calculate saturation */
+		saturation = (lightness > 0.5) ? chroma / (2.0 - max - min) : chroma / (max + min);
 
-			/* calculate color */
-            hue = Hue_From_Color(red, green, blue, max, chroma);
-		}
+		/* calculate color */
+                hue = Hue_From_Color(red, green, blue, max, chroma);
+	}
 
-		/* scale the hue from distance to distance in degrees by multiplying by 60 */
+	/* scale the hue from distance to distance in degrees by multiplying by 60 */
         hsl.Hue = hue * 60.0;
-		hsl.Saturation = saturation;
-		hsl.Lightness = lightness;
+	hsl.Saturation = saturation;
+	hsl.Lightness = lightness;
 
         return hsl;
     }
@@ -331,20 +426,20 @@ namespace Color {
         HSV hsv;
 
         /* scale our values to between [0, 1]
-		 * cast our vaules to T to ensure the compiler interprets it correctly
-		 */
-		double red = static_cast<double>(rgb.Red) / 255.0;
-		double green = static_cast<double>(rgb.Green) / 255.0;
-		double blue = static_cast<double>(rgb.Blue) / 255.0;
+         * cast our vaules to T to ensure the compiler interprets it correctly
+         */
+	double red = static_cast<double>(rgb.Red) / 255.0;
+	double green = static_cast<double>(rgb.Green) / 255.0;
+	double blue = static_cast<double>(rgb.Blue) / 255.0;
 
-		double max = tmax3<double>(red, green, blue);
-		double min = tmin3<double>(red, green, blue);
+	double max = tmax3<double>(red, green, blue);
+	double min = tmin3<double>(red, green, blue);
 
-		/* calculate chroma */
-		double chroma = max - min;
+	/* calculate chroma */
+	double chroma = max - min;
 
         double hue;
-		double saturation;
+	double saturation;
 
         double value = max;
 
@@ -353,7 +448,7 @@ namespace Color {
             saturation = 0.0;
         } else {
             /* calculate saturation */
-			saturation = (max > 0.0) ? chroma / max : 0.0;
+	    saturation = (max > 0.0) ? chroma / max : 0.0;
 
             /* calculate color */
             hue = Hue_From_Color(red, green, blue, max, chroma);
@@ -361,8 +456,8 @@ namespace Color {
 
         /* scale the hue from distance to distance in degrees by multiplying by 60 */
         hsv.Hue = hue * 60.0;
-		hsv.Saturation = saturation;
-		hsv.Value = value;
+	hsv.Saturation = saturation;
+	hsv.Value = value;
 
         return hsv;
     }
@@ -397,6 +492,8 @@ using namespace Color;
 int main(void) {
     HSL hsl(34.0, 0.3, 0.5);
     HSV hsv;
+    RGB rgb;
+    CMYK cmyk;
 
     hsv = hsl;
 
@@ -407,6 +504,12 @@ int main(void) {
     hsl = hsv;
 
     std::cout << hsl << std::endl;
+
+    rgb = hsl;
+
+    cmyk = rgb;
+
+    std::cout << cmyk << std::endl;
 
     return 0;
 }
